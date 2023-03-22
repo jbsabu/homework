6e7455b2b9b72056dd7d4adcdc4c29a71a6a8a94
Jonathan B. Sabuncu <sundfo057@gmail.com>
2023-03-22 10:21:30 -0400

    HW W1D3

---
+++ variables.js
@@ -0,0 +1,7 @@
+const country = "USA"
+const continent = "North America"
+
+console.log(country,continent)
+
+let favoriteFood = "Sushi"
+console.log("'15' ===  15 would be false due to mismatched datatype")   
---
+++ weekOneDayTwo.js
@@ -0,0 +1,38 @@
+const animals = ['bird','cat','dog']
+
+function animalSound(animal){
+    let sound = undefined
+    switch (animal){
+        case "cat":
+            sound = "meow"
+            break
+            case 'dog':
+            sound = "woof"
+            break
+            case 'bird':
+                sound = "chirp"
+            break
+            default:
+
+            sound = "none"
+    }
+    return sound
+}
+
+console.log(animalSound('bird'))
+
+const fruits = {apple: 'red',banana: 'yellow',mango:'green',kiwi:'green'}
+
+function getFruitColor(fruit){
+    if (fruits[fruit]) {
+        return fruits[fruit]
+    }
+}
+
+console.log(getFruitColor('kiwi'))
+
+function getSum(a,b){
+    return a+b
+}
+
+console.log(getSum(1,3.14))
