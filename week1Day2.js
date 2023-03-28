12ebc2cd4d286961a25b04e2acdb87fd66036069
Jonathan B. Sabuncu <sundfo057@gmail.com>
2023-03-28 09:04:04 -0400

    week1day2

---
+++ week1Day2.js
@@ -0,0 +1,12 @@
+let myCar = {brand: "Toyota", make: "Prius", color: "red"}
+
+let {brand,make,color} = myCar
+
+console.log(color)
+
+
+const hourToSecond = (hours)=>{
+return Math.floor(hours*60)
+}
+
+console.log(hourToSecond(54))
